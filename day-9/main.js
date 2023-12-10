985af99d95b73dd2bb5b3a1ce4e8e2a99586a9cf
Nicolas PIERRE <npierre9@yahoo.fr>
2023-12-10 19:28:13 +0100

    Solution for part 2 of day 9 of advent of code plus a short explanation on what was the reasoning behind this solution in comments

--- day-9/main.js
+++ day-9/main.js
@@ -20,6 +20,12 @@ const makeDifferencesList = (list) => {
     return differencesList
 }
 
+
+/*
+We actually never need to construct the new element of every difference list, 
+since we can prove that the next entry will be the sum of the last element of each difference list plus the last term of the list we are extrapolating
+This is an easy proof by induction with the assumptions made in this problem.
+*/
 const findNextEntry = (list) => {
     var differencesList = structuredClone(list)
     var nextEntry = 0
@@ -42,3 +48,33 @@ console.log(main())
 
 
 ////////////// PART 2 /////////////////
+
+/*
+We proceed nearly the same way than in part 1, except this time the solution is the sum of terms but with an alternating sign depending on the parity of the difference list number
+(if the list is the (2*n+1)th one, it is substracted, it is added otherwise)
+*/
+
+const findPreviousEntry = (list) => {
+    var differencesList = structuredClone(list)
+    var previousEntry = Number(list[0])
+    for (let i=0; i< list.length -1; i++) {
+        differencesList = makeDifferencesList(differencesList)
+        if (i%2 == 0) {
+            previousEntry = previousEntry - Number(differencesList[0])
+        } else {
+            previousEntry = previousEntry + Number(differencesList[0])
+        }
+    }
+    return previousEntry
+}
+
+
+const mainPartTwo = () => {
+    answer = 0
+    for (let i=0; i<history.length; i++) {
+        answer = answer + findPreviousEntry(history[i])
+    }
+    return answer
+}
+
+console.log(mainPartTwo())
